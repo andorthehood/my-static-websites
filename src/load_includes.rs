2ea16f081287160705e26754a2480b0416c6b663
andormade <andorsigned@gmail.com>
2025-08-02 22:57:09 +0000

    fix(core): fix snapshot test

--- src/load_includes.rs
+++ src/load_includes.rs
@@ -30,7 +30,7 @@ mod tests {
 
     #[test]
     fn test_load_liquid_includes() {
-        let templates = load_liquid_includes("./sites/lepkef.ing/includes");
+        let templates = load_liquid_includes("./sites/test/includes");
         let sorted_templates: BTreeMap<_, _> = templates.into_iter().collect();
         let templates_json = serde_json::to_string_pretty(&sorted_templates)
             .expect("Failed to serialize templates to JSON");
