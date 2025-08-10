16dac53c4a121fdbbcbc365a05545a9eb363a0de
andormade <hngrhorace@gmail.com>
2025-08-10 21:27:24 +0000

    feat(lepkef.ing): add clippy animations

--- sites/lepkef.ing/assets/script.js
+++ sites/lepkef.ing/assets/script.js
@@ -61,3 +61,11 @@ document.getElementById('button-print').addEventListener('click', function () {
         y = Math.min(y, maxY);
     });
 })();
+
+(function() {
+    const clippyGif = document.getElementById('clippy-gif');
+
+    setInterval(function() {
+        clippyGif.src = 'https://static.llllllllllll.com/andor/assets/clippy/swaying.gif?c=' + Date.now();
+    }, 8000);
+})();
